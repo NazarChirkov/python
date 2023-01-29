1d1088e838905d75badf965a1e928acb7b7bff28
NazarChirkov <nazarchirkov@icloud.com>
2023-01-29 17:49:41 +0000

    v0.0.9

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ def create_table(connection):
         cursor = connection.cursor()
         create_table_query = """
         CREATE TABLE students (
-        student_id INT PRIMARY KEY,
-<<<<<<< HEAD
-        name VARCHAR(20),
-=======
-=
-        major VARCHAR(20)
+        student_id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(20) DEFAULT 'no name',
+        major VARCHAR(20) NOT NULL
         );
         """
+        cursor.execute(create_table_query)
         print("Table created seccesfully")
-def create_user(connection):
+def create_user(connection,table,name,major):
         cursor = connection.cursor()
+        create_user_query = """INSERT INTO %s(name,major) VALUES ('%s','%s')""" % (table,name,major)
+        cursor.execute(create_user_query)
         connection.commit()
 def show_all_rows(connection):
         cursor = connection.cursor()
-        cursor.execute("""SELECT * FROM userB""")
+        cursor.execute("""SELECT * FROM students""")
         rows = cursor.fetchall()
         for row in rows:
             print(row)
@@ -34,6 +34,8 @@ def mySQL():
         database=db_name,
         cursorclass=pymysql.cursors.DictCursor
     )
+    create_user(connection,"students", "Oleg", "UK")
+    show_all_rows(connection)
     connection.close()
  except Exception as ex:
     print(ex)
